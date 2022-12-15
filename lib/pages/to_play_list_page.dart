e2fc9f5f957e8d811764f27bb5b6be18e38e9d76
Ayşe Zeynep Subaşı <subasi19@itu.edu.tr>
2022-12-15 16:07:10 +0300

    started design of to play list page

--- lib/pages/to_play_list_page.dart
+++ lib/pages/to_play_list_page.dart
@@ -5,6 +5,69 @@ class ToPlayListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: const Color(0xFF1F1D36),
+        alignment: Alignment.center,
+        child: SafeArea(
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              const SizedBox(height: 25,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(),
+                        ),
+                        const Expanded(
+                          flex: 1,
+                          child: Text(
+                            'To Play List',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            child: const Icon(
+                              Icons.filter_alt,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                height: 60,
+                child: Divider(
+                  color: Colors.white.withOpacity(0.19),
+                  thickness: 1.2,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 13),
+                child: Container(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
