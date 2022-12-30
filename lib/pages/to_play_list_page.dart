5f3dcd907531c0ed4e9ff6b4d0e6e4d28ed46d69
Ayşe Zeynep Subaşı <56104487+ituitis-subasi19@users.noreply.github.com>
2022-12-30 18:08:07 +0300

    to play list handled

--- lib/pages/to_play_list_page.dart
+++ lib/pages/to_play_list_page.dart
@@ -1,7 +1,44 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class ToPlayListPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:gameinn/model/game_model.dart';
+import 'package:gameinn/model/user_model.dart';
+import 'package:gameinn/pages/game_details.page.dart';
+import 'package:gameinn/service/search_service.dart';
+import 'package:gameinn/service/user_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ToPlayListPage extends StatefulWidget {
   const ToPlayListPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ToPlayListPageState();
+}
+
+class _ToPlayListPageState extends State<ToPlayListPage> {
+  List<GameModel?> games = [];
+  String _userid = "";
+  UserModel _user = UserModel(id: "");
+
+  @override
+  void initState() {
+    getUser();
+    super.initState();
+  }
+
+  void getUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    UserModel user = UserModel.fromJson(jsonDecode((prefs.getString('user'))!));
+
+    List<GameModel> tempList =
+        (await UserService().toPlayList(user_id: user.id!))!;
+
+    setState(() {
+      _userid = user.id!;
+      _user = user;
+      games = tempList;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +50,9 @@ class ToPlayListPage extends StatelessWidget {
           child: ListView(
             scrollDirection: Axis.vertical,
             children: [
-              const SizedBox(height: 25,),
+              const SizedBox(
+                height: 25,
+              ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
@@ -62,14 +101,56 @@ class ToPlayListPage extends StatelessWidget {
               ),
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 13),
-                child: GridView.count(
+                child: GridView.builder(
                   shrinkWrap: true,
                   scrollDirection: Axis.vertical,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  crossAxisCount: 3,
-                  childAspectRatio: 0.6,
-                  children: toPlayList,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    childAspectRatio: 0.6,
+                  ),
+                  itemCount: games.length,
+                  itemBuilder: (context, index) {
+                    GameModel? game = games[index];
+                    return InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => GameDetailsPage(
+                                      game_id: game.id!,
+                                    )));
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 7,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(5.0),
+                              child: Image.memory(
+                                base64Decode((game?.cover)!),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Center(
+                              child: Text(
+                                (game!.name!),
+                                style: const TextStyle(
+                                  fontSize: 13.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
@@ -96,15 +177,15 @@ List<Widget> toPlayList = <Widget>[
       ),
       const Expanded(
         flex: 1,
-          child: Center(
-            child: Text(
-              "Forgotton Anne",
-              style: TextStyle(
-                fontSize: 13.0,
-                color: Colors.white,
-              ),
+        child: Center(
+          child: Text(
+            "Forgotton Anne",
+            style: TextStyle(
+              fontSize: 13.0,
+              color: Colors.white,
             ),
           ),
+        ),
       ),
     ],
   ),
